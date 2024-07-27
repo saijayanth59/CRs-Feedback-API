278c7040037322305fc71aa8b03278bb98468cbc
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-07-27 12:05:56 +0530

    create filters

---
+++ feedbacks/filters.py
@@ -0,0 +1,15 @@
+from django_filters import rest_framework as filters
+from .models import Feedback
+
+class FeedbackFilter(filters.FilterSet):
+    section = filters.CharFilter(field_name='user__section', lookup_expr="iexact")
+    batch = filters.CharFilter(field_name="user__year", lookup_expr="iexact")
+    id_number = filters.CharFilter(field_name='user__username', lookup_expr='iexact')
+    year = filters.NumberFilter(field_name='created__year')
+    month = filters.NumberFilter(field_name='created__month')
+    day = filters.NumberFilter(field_name='created__day')
+    date = filters.DateFilter(field_name='created__date')
+
+    class Meta:
+        model = Feedback
+        fields = '__all__'
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,15 +1,20 @@
 from .models import Feedback, User
 from .serializers import FeedbackSerializer
+from .filters import FeedbackFilter
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class FeedbackListCreate(generics.ListCreateAPIView):
-    serializer_class = FeedbackSerializer
     queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    filter_backends = [DjangoFilterBackend] 
+    filterset_class = FeedbackFilter
+    filterset_fields = '__all__'
     
     def perform_create(self, serializer):
         try:
-            user = User.objects.get(username=self.request.POST['id_number'])
+            user = User.objects.get(username=self.request.data['id_number'])
         except:
             raise ValueError("Go away!")
         serializer.save(user=user)
